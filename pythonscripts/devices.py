e672e6d4a47d64c8972de35f5a145d4ed4c413cf
Jolly Jae Ompod <jollyjae.ompod@icloud.com>
2023-11-08 23:43:47 +0100

    devices.py: added getter and setter

--- pythonscripts/devices.py
+++ pythonscripts/devices.py
@@ -2,16 +2,11 @@
 
 class Devices:
     def __init__(self, device_name, username, password, ip_address, command):
-        self.device_name = device_name
-        self.username = username
-        self.password = password
-        self.ip_address = ip_address
-        self.command = command
-    
-    @classmethod
-    def default_arduino(cls):
-        return cls("Arduino", "jollyjae", "password", "192.168.0.162", "ping -c5")
-
+        self._device_name = device_name
+        self._username = username
+        self._password = password
+        self._ip_address = ip_address
+        self._command = command
 
     def get_device_info(self):
         print(f"Device Info => Device Name: {self.device_name}, ip Address: {self.ip_address}")
@@ -26,19 +21,36 @@ class Devices:
             self._device_name = new_device_name
         else:
             print("Name must be a string.")
-        
     
-    def set_username(self, username):
-        self.username = username
-        
-        
-    def set_password(self, password):
-        self.password = password
-        
-        
-    def set_ip_address(self, ip_address):
-        self.ip_address = ip_address
-        
-        
-    def set_command(self, command):
-        self.command = command
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, new_username):
+        if isinstance(new_username, str):
+            self._username = new_username
+        else:
+            print("Username must be a string.")
+            
+    @property
+    def ip_address(self):
+        return self._ip_address
+    
+    @ip_address.setter
+    def ip_address(self, new_ip_address):
+        if isinstance(new_ip_address, str):
+            self._ip_address = new_ip_address
+        else:
+            print("IP Address must be a string.")
+    
+    @property
+    def command(self):
+        return self._command
+    
+    @command.setter
+    def command(self, new_command):
+        if isinstance(new_command, str):
+            self._ip_address = new_command
+        else:
+            print("Command must be a string.")
