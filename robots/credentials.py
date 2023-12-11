d77a53e3f86367a189945de81ce1b5a8c6e27852
Jolly Jae Ompod <jollyjae.ompod@icloud.com>
2023-12-11 16:58:31 +0100

    credential.py : removing credential.

--- robots/credentials.py
+++
@@ -1,8 +0,0 @@
-ARDUINOYUN_IP    = '192.168.0.162'
-ARD_USERNAME     = 'root'
-ARD_PASSWORD     = 'arduino'
-
-RASPBERRYPI4_IP  = '192.168.0.186'
-RAS_USERNAME     = 'jollyjae'
-RAS_PASSWORD     = 'Bondoy1949@'
-RAS_PORT         = '1979'
